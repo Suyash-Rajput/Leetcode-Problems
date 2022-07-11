c6f8263d9d4843c6f585aadff6cfdd2a3e407a7f
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-07-11 08:06:44 +0530

    Time: 7 ms (46.19%), Space: 12 MB (44.63%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+         vector<int> v ;
+        if(!root) return v ;
+       
+        queue<TreeNode *> q ;
+        q.push(root) ;
+        while(!q.empty()) {
+          int a = q.size() ;
+            for(int i=0;i<a;i++) {
+                TreeNode * curr= q.front() ;
+                q.pop() ;
+              if(i==0) {
+                  v.push_back(curr->val) ;
+              }
+                
+               if(curr->right) q.push(curr->right) ;
+             if(curr->left)  q.push(curr->left) ;
+            }
+            
+       }
+        
+        return v ;
+    }
+};
