b82c45a73669fdde150837879061b0696ee2e99e
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-07-15 22:21:32 +0530

    Time: 23 ms (79.42%), Space: 23.3 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -11,7 +11,7 @@ public:
         int m = grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]){
+                if(grid[i][j]==1){
                     ans=max(ans,helper(grid,i,j));
                 }
             }
