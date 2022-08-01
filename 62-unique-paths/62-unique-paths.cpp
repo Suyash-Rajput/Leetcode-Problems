efd5f8c1b82b4622db480c104ce1cbd354481a89
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-01 14:00:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -13,7 +13,7 @@ public:
         
         for(int i= 1;i<m;i++) {
             for(int j=1;j<n;j++) {
-                arr[i][j]=arr[i][j-1]+arr[i-1][j] ;
+                arr[i][j]=arr[i-1][j]+arr[i][j-1] ;
             }
         }
         
