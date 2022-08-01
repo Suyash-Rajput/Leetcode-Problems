62aeadcd72953968d16bf4ec88f043b09da7bb33
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-01 16:27:49 +0530

    Time: 9 ms (49.56%), Space: 7.7 MB (39.00%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-         string b ;
-        for(int i=0;i<s.size();i++) {
-          if(s[i]>='a' && s[i]<='z')
+        string b  ;
+        for(int i=0 ;i<s.size();i++) {
+            if(s[i]>='a' && s[i]<='z'){
+                b+=s[i] ;
+            }
+            if(s[i]>='A' && s[i]<='Z')
+                b+=tolower(s[i]) ;
+            if(s[i]>='0' &&s[i]<='9')
             b+=s[i];
-        else if(s[i]>='A'&& s[i]<='Z')
-                b+=tolower(s[i]);
-        else if(s[i]>='0' && s[i]<='9')
-            b+=s[i];
- 
+            
+        }
+        int a =b.size();
+        for(int i=0;i<a/2;i++){
+            if(b[i]!=b[a-i-1]){
+                return false ;
+            }  
         }
         
-        int a = b.size() ;
-        for(int i =0;i<a/2;i++) {
-            if(b[i]!=b[a-i-1]) return false ;
-          
-        }
-          return true ;
+        
+        
+        
+        return true ;
     }
 };
