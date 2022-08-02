a00d8a1a0dfe31f3cbb507e028a2b6c80852adb8
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-02 16:14:04 +0530

    Time: 0 ms (100.00%), Space: 58.2 MB (92.46%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -5,15 +5,17 @@ class Solution {
         reverse(nums , 0 , nums.length-1);
         reverse(nums , 0 , k-1);
         reverse(nums , k , nums.length -1);
+         
     }
-      public static void reverse(int[] arr , int start , int end){
-        while(start<end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    public static void reverse(int[] arr,int start,int end) {
+        while(start<end) {
+            int temp =arr[start] ;
+            arr[start ] =arr[end ]; 
+            arr[end] =temp ;
+            start++ ;
+            end-- ;
         }
-    }   
+    }
+      
           
 }
