3ccd0263d973e6553f2261027e802067572f21fd
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-05 11:35:09 +0530

    Time: 35 ms (43.33%), Space: 13.2 MB (5.76%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -7,7 +7,7 @@ public:
             m[nums[i]]=i  ;
         }
         for(int i=0;i<nums.size();i++) {
-            if(m[target-nums[i]]!=0 && m[target-nums[i]]!=i){
+            if(m[target-nums[i]] && m[target-nums[i]]!=i){
                 v.push_back(i) ;
                 v.push_back(m[target-nums[i]]) ;
                 break ;
