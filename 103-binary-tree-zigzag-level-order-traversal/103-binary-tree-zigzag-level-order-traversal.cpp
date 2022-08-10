2699090b390128618e0bc3deb59a9b20fec8e027
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-10 20:23:21 +0530

    Time: 3 ms (81.47%), Space: 12 MB (76.11%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,31 +12,29 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result ;
-        if(root==NULL) return result ;
-        
-        queue<TreeNode*>  nodeQueue ;
-        nodeQueue.push(root ) ;
-        bool lefttoright=true;
-      
-        while(!nodeQueue.empty()) {
-            int size=  nodeQueue.size() ;
-            vector<int> row(size) ;
-            for(int i=0;i<size;i++) {
-                TreeNode *node = nodeQueue.front() ;
-                nodeQueue.pop();
-                int index = (lefttoright)? i:(size-1-i) ;
-                row[index] =node->val ;
-                if(node->left) 
-                    nodeQueue.push(node->left) ;
+          vector<vector<int>> v; 
+        if(root==NULL)return v ;
+        queue<TreeNode* > q; 
+       
+        q.push(root) ;
+        bool left =true ;
+        while(!q.empty()){
+            int a =q.size() ;
+            vector<int> row(a) ;
+            for(int i=0;i<a;i++) {
+                 TreeNode *node =q.front() ;
+                q.pop();
+                 int index = (left )? i:(a-1-i) ;
+                 row[index ] =node->val ;
+                if(node->left)
+                    q.push(node->left) ;
                 if(node->right) 
-                    nodeQueue.push(node->right) ;
-                
-            }
-            lefttoright=!lefttoright;
-            result.push_back(row) ;
+                    q.push(node->right);
+            }            
+            left =!left ;
+            v.push_back(row) ;
         }
-        
-    return result;   
+       
+return v ;        
     }
 };
