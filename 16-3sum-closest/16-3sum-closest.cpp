51af41fa959401e2fd4c37422d34e613139c9c86
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-12 10:18:37 +0530

    Time: 257 ms (80.01%), Space: 16.4 MB (71.93%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
+  
     int threeSumClosest(vector<int>& nums, int target) {
-                int ans = INT_MAX;
-        int dj = 0;
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for(int i = 0;i<n;i++){
-            int tar = target-nums[i];
-            int s = i+1;
-            int e = n-1;
-            while(s<e){
-                int sum = nums[e]+nums[s];
-                int js= sum+nums[i];
-                if(sum==tar) return target;
+          int ans =INT_MAX ;
+        int dj= 0 ;
+        sort(nums.begin(),nums.end()) ;
+        
+        int n = nums.size() ;
+        for(int r=0;r<n;r++) {
+            int tar =target-nums[r] ;
+            int i = r+1; 
+            int j =n-1 ;
+            while(i<j) {
+                int sum = nums[i]+nums[j] ;
+                int js= sum+nums[r] ;
+                if(sum==tar) return target ;
                 else if(sum>tar){
-                    e--;
-                    if(abs(target-js)<ans){
-                        ans = min(ans,abs(target-js));
-                        dj = js;
+                     j-- ;
+                    if(abs(target-js)<ans) {
+                        ans =min(ans,abs(target-js) ) ;
+                        dj = js ;
                     }
                 }
-                else {
-                    s++;
-                    if(abs(target-js)<ans){
-                        ans = min(ans,abs(target-js));
-                        dj = js;
+                else{
+                    i++ ;
+                    if(abs(target-js)<ans) {
+                        ans =min(ans,abs(target-js) ) ;
+                        dj = js ;
                     }
                 }
-                
             }
-            while(i+1<n && nums[i]==nums[i+1]) i++;
+            while(i+1<n &&nums[i]==nums[i+1]) i++;
         }
-        return dj;
-    }
+     return dj ;
 
+        
+    }
 
 };
