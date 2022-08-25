a688fcae9bc44e74beb0c659ffb2d8a265d06e5e
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-25 23:24:55 +0530

    Time: 78 ms (34.53%), Space: 17.8 MB (50.68%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -11,45 +11,46 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-       if(head==NULL ||head->next==NULL) return ;
+        if(!head ||head->next==NULL ) return ;
+        ListNode *l1= head ; 
+       ListNode *slow = head ;
+        ListNode *fast =head; 
+        ListNode *prev=NULL ;
         
-        ListNode *slow =head;
-        ListNode *prev= slow ;
-        ListNode *fast =head ;
-        while(fast!=NULL && fast->next!=NULL){
-            prev=slow ;
-            slow=slow->next ;
-            fast=fast->next ->next ;
+        while(fast!=NULL && fast->next!=NULL) {
+            prev =slow ;
+            slow =slow->next; 
+            fast =fast->next->next ;
         }
         prev->next=NULL ;
-        ListNode * l1 =head;
-        ListNode *l2= reverse(slow) ;
-          
+       ListNode *l2= reverse(slow) ;  
+     
         merge(l1,l2) ;
-    }
-  ListNode *reverse(ListNode *head){
-      if(!head)return NULL ;
-      ListNode *temp =head ;
-      ListNode *prev=NULL;
-      ListNode *next=NULL; 
-      while(temp!=NULL){
-          next =temp->next ;
-          temp->next=prev;
-          prev= temp;
-          temp = next; 
-      }
-      return prev; 
-      
-  }
-   void merge(ListNode *l1,ListNode *l2) {
-       while(l2!=NULL) {
-           ListNode *p =l1->next ;
-           l1->next =l2 ;
-           l1=l2 ;
-           l2=p ;
+   } 
+    
+   ListNode * reverse(ListNode *root) {
+       if(!root) return NULL ;
+       ListNode *next =NULL; 
+       ListNode *prev=NULL;
+       ListNode *temp=root; 
+       while(temp!=NULL) {
+           next =temp->next ;
+            temp->next= prev ;
+             prev=temp ;
+           temp=next ;
        }
-       
+      return prev ;
    }  
+    void merge(ListNode *l1,ListNode *l2) {
+        while(l2!=NULL) {
+        ListNode *r =l1->next ;
+        l1->next=l2 ;
+        l1= l2 ;
+        l2 =r ;
+            
+    }
+    }
+    
      
 };
  
