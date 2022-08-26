5f002d62392c7a84fb019f6b3abc6e86323ee7f3
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-26 10:02:17 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.18%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> suy ;
+        
+        if(s.size()%2!=0) 
+            return false ;
+        
+        for(int i=0;i<s.size();i++) {    
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[' ){
+                suy.push(s[i]) ;
+            }
+            else if(suy.empty()) return false ;
+            else{
+                 
+                if(suy.top()=='('){
+                     if(s[i]!=')') 
+                         return false ;
+                     suy.pop();    
+                }
+               else  if(suy.top()=='['){
+                     if(s[i]!=']') 
+                         return false ;
+                     suy.pop();    
+                }
+              else  if(suy.top()=='{'){
+                     if(s[i]!='}') 
+                         return false ;
+                     suy.pop();    
+                }
+            }
+            
+        }
+     return suy.empty()? true:false ;
+    }
+};
+
+ 
