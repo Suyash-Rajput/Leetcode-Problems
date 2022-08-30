ff4a6cdb4197d7bb4351332c415ee3b2dc3558f1
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-30 10:18:46 +0530

    Time: 11 ms (63.97%), Space: 15.1 MB (7.85%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,48 +11,30 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2==NULL) return NULL; 
-        if(list1==NULL) return list2; 
-        if(list2==NULL) return list1 ;
-        ListNode *res  ;
-        if(list1->val > list2->val) {
-            res= new ListNode(list2->val) ;
-            list2=list2->next ;
-        }
-        else{
-            res = new ListNode(list1->val) ;
-            list1=list1->next ;
-        }
-        ListNode *temp =res ;
-        while(list1!=NULL && list2!=NULL) {
-            if(list1==NULL){
-                while(list2!=NULL) {
-                    temp->next =new ListNode(list2->val) ;
-                 temp=temp->next ;
-                    list2=list2->next ;
-                }
-            }
-            if(list2==NULL) {
-                while(list1!=NULL) {
-                    temp->next =new ListNode(list1->val) ;
-                    temp=temp->next ;
-                    list1=list1->next ;
-                }
-            }
-            if(list1->val>list2->val) {
-                temp->next = new ListNode(list2->val) ;
-                temp=temp->next ;
-                list2=list2->next ;
+     ListNode* ans = new ListNode();
+        ListNode* temp=ans;
+        while(list1!=NULL and list2!=NULL){
+            if(list1->val<=list2->val){
+                ans->next=new ListNode(list1->val);
+                list1=list1->next;
             }
             else{
-                temp->next = new ListNode(list1->val) ;
-                temp=temp->next ;
-                list1=list1->next ;
+                ans->next = new ListNode(list2->val);
+                list2=list2->next;
             }
+            ans=ans->next;
         }
-        
-        if(list1!=NULL)temp->next = list1;
-        if(list2!=NULL)temp->next = list2;
-        return res;
+        if(list1!=NULL){
+            ans->next=list1;
+        }
+        if(list2!=NULL){
+            ans->next=list2;
+        }
+        return temp->next;
     }
 };
+
+
+
+
+
