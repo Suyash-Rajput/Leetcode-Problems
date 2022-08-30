0d07bce622efc49271ab639be1f8dccf17f5dc7c
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-30 22:22:03 +0530

    Time: 532 ms (47.80%), Space: 16.3 MB (65.00%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -30,7 +30,8 @@ public:
                     }
                 }
             }
-            while(i+1<n &&nums[i]==nums[i+1]) i++;
+            //while(i+1<n &&nums[i]==nums[i+1]) i++;
+            
         }
      return dj ;
         
