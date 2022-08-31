4cb6ae8e2e53c7ade81b9a969d2adba5b0ee339b
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-08-31 18:29:45 +0530

    Time: 25 ms (55.17%), Space: 17 MB (75.37%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-     int intervLen = intervals.size();
-        int newIntervLen = newInterval.size();
-         
-        vector<vector<int>> ans;
-        int i=0;
-        int currMin = newInterval[0];
-        int currMax = newInterval[1];
-        
-        // left which don't overlap
-        while(i<intervLen and intervals[i][1]<currMin){ 
-            ans.push_back(intervals[i]);
-            i++;
+     int a = intervals.size();
+    vector<vector<int>> v ;
+       int  i= 0;
+        int currmin = newInterval[0] ;
+        int currmax =newInterval[1] ; 
+        while(i<intervals.size() && intervals[i][1]<currmin){
+            v.push_back(intervals[i]) ;
+            i++ ;
+        }
+        while(i<intervals.size() && intervals[i][0] <=currmax) {
+            currmin= min(currmin,intervals[i][0]) ;
+            currmax =max(currmax,intervals[i][1]) ;
+            i++ ;
+        }
+    v.push_back({currmin,currmax}) ;
+        while(i<intervals.size() ){
+            v.push_back(intervals[i]) ;
+             i++;
         }
         
-        // overlap each other keep track of min and max 
-        while(i<intervLen and intervals[i][0] <= currMax){
-            currMin = min(currMin, intervals[i][0]);
-            currMax = max(currMax, intervals[i][1]);
-            i++;
-        }
-        //push final to ans
-        ans.push_back({currMin, currMax});
         
-        // right who do not overlap
-        while(i<intervLen){
-            ans.push_back(intervals[i]);
-            i++;
-        }
-        
-        return ans;
+        return v ;
     }
 };
