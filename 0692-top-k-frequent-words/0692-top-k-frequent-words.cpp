57a4df57d9a64d2566ddcd16eb1a5236fb88a5c9
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-10-21 14:43:56 +0530

    Time: 17 ms (80.98%), Space: 12.8 MB (40.57%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,29 +1,33 @@
-struct Compare {
-    bool operator() (pair<int, string> a, pair<int, string> b) {
-        if(a.first == b.first)
-            return a.second > b.second;
-        else
-            return a.first < b.first;
+struct compare{
+    bool operator()(pair<int,string> a,pair<int,string> b) {
+        if(a.first==b.first )  
+            return  a.second>b.second ;
+        else{
+            return  a.first <b.first ;
+        }
+        
     }
+   
 };
-
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> m;
-        for(int i=0; i<words.size(); i++)
-            m[words[i]]++;
-        
-        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> q;
-        for(auto p : m)
-            q.push({p.second, p.first});
-        
-        vector<string> ans;
-        while(k--) {
-            ans.push_back(q.top().second);
-            q.pop();
+        unordered_map<string,int> m ;
+        for(int  i=0;i<words.size();i++) {
+            m[words[i]]++ ;
+            
         }
+  priority_queue<pair<int, string>, vector<pair<int, string>>, compare> p;
+        for(auto f:m) 
+               p.push({f.second,f.first}) ;
         
-        return ans;
+        vector<string> v; 
+        
+        while(k-- ){
+            v.push_back(p.top().second ) ;
+            p.pop() ;
+        }
+         
+        return v ;
     }
 };
