033782520d090154bb4b4f13e7f7c95d6758d43e
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-10-31 09:29:38 +0530

    Time: 50 ms (44.87%), Space: 13.3 MB (5.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+         
         vector<int> v ;
-        map<int,int> m ; 
-        for(int i =0 ;i<nums.size();i++) {
-            m[nums[i]]=i ;
-        }
+        map<int,int> m ;
         
         for(int i=0;i<nums.size();i++) {
-            if(m[target-nums[i]] && m[target-nums[i]]!=i){
-                v.push_back(i) ; 
-               v.push_back(m[target-nums[i]] ) ; 
-                break;
+          m[nums[i]]=i  ;
+         }
+        
+        for(int i=0;i<nums.size();i++) {
+            if(m[target-nums[i]] && m[target-nums[i]]!=i) {
+                v.push_back(i) ;
+                v.push_back(m[target-nums[i]]) ;
+                break ;
             }
             
         }
         
-        
-        
         return v; 
+        
     }
 };
