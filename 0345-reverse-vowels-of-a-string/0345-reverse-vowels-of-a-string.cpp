2e706eb6bff895da0d6b5d72bab9ffff239938c5
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-11-04 11:19:55 +0530

    Time: 23 ms (23.09%), Space: 7.5 MB (99.94%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -11,24 +11,24 @@ public:
             return false ;
     }
     
+    
     string reverseVowels(string s) {
-  
-        vector<char> v ;
-        for(int i=0;i<s.size();i++) {
-            char t =s[i] ;
-            if(check(t)) {
-                v.push_back(t) ;
+      int  n = s.size() ;
+        int i=0 ;
+        int j = n-1 ;
+        
+        while(i<=j&&i!=n&&j!=-1  ){
+            while(i<=j &&i!=n&&j!=-1 && !check(s[i])) i++ ;
+            while(i<=j &&i!=n &&j!=-1&&!check(s[j])) j-- ;
+            
+            if(i<=j &&i!=n&&j!=-1 ) {
+                swap(s[i],s[j]) ;
             }
+            i++;
+            j--;
+            
         }
-     for(int i=0;i<s.size();i++) {
-        if(check(s[i])) {
-          s[i] =v.back() ;
-            v.pop_back() ;
-        }
-         
-     }   
         
-        return s ; 
-        
+        return s ;
     }
 };
