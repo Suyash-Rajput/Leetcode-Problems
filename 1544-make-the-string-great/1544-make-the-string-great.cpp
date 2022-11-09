988fad7ba05661621b5bb01f474fd2335bde7389
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2022-11-09 21:39:27 +0530

    Time: 5 ms (54.40%), Space: 6.3 MB (65.54%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string makeGood(string s) {
-        string ans;
+       string ans ;
         
-        for(int i = 0 ; i < s.size() ; i++)
-        {
-            ans.push_back(s[i]);
-            
-            while(ans.size() && (ans.back()==s[i+1]+32 || ans.back()==s[i+1]-32))
-            {
-                ans.pop_back();
-                i++;
+        for(int i=0;i<s.size();i++) {
+           ans.push_back(s[i]); 
+            while(ans.size() && (ans.back()==s[i+1]+32 || ans.back()==s[i+1]-32) ) {
+                ans.pop_back() ;
+                i++ ;
             }
         }
         
-        return ans;
+        
+     return ans ;
     }
 };
